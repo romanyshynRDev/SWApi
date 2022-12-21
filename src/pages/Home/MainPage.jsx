247b71bad034033154e44fbc70095d01c5ca0ce7
unknown <roman1romanyshyn@gmail.com>
2022-12-21 15:37:05 +0200

    react-create-app on Heroku

---
+++ src/pages/Home/MainPage.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMoviesList } from "../../Service/ApiService";
+import FilmContainer from "./HomePage";
+
+const MainPage = () => {
+  const { list, isLoading } = useSelector(state => state.movies)  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getMoviesList())
+  }, [dispatch])
+  
+  return(
+    <FilmContainer list={list} isLoading={isLoading} />
+  )
+}
+export default MainPage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
