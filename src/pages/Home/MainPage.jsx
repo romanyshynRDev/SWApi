6dd34889775d3e1574a749c2c1bef835d7eaca20
Roman <roman1romanyshyn@gmail.com>
2025-03-03 18:00:06 +0200

    fixed tests

--- src/pages/Home/MainPage.jsx
+++ src/pages/Home/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMoviesList } from "../../Service/ApiService";
 import FilmContainer from "./HomePage";
