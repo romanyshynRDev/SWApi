a0038895503dc248897cfd3b310dadef9da08de2
Roman <78535046+romanyshyn001@users.noreply.github.com>
2025-03-03 19:11:26 +0200

    Feature 2 (#6)
    * feature
    
    * added bug
    
    * added func
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fix 1
    
    * fixed error
    
    * fixed tests
    
    * fixed error
    
    * updated changes
    
    * fixed tests
    
    * new feature add
    
    * fixed bug

--- src/pages/Home/MainPage.jsx
+++ src/pages/Home/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMoviesList } from "../../Service/ApiService";
 import FilmContainer from "./HomePage";
@@ -7,12 +7,18 @@ const MainPage = () => {
   const { list, isLoading } = useSelector((state) => state.movies);
   const dispatch = useDispatch();
 
+const [increment, setIncrement] = useState(0)
+
   useEffect(() => {
     dispatch(getMoviesList());
   }, [dispatch]);
 
   return (
     <>
+
+    {increment}
+    <button onClick={() => {setIncrement((prev) =>prev+1)}}>increment</button>
+
       <FilmContainer list={list} isLoading={isLoading} />
     </>
   );
