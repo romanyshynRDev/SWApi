f9e433e5a208dac72dd9c50091bc3aab2676e118
Roman <roman1romanyshyn@gmail.com>
2025-03-03 16:43:41 +0200

    created test

---
+++ src/__tests__/FilmContainer.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import FilmContainer from '../pages/Home/HomePage';
+
+const renderWithRouter = (component) => {
+  return render(<BrowserRouter>{component}</BrowserRouter>);
+};
+
+describe('FilmContainer Component', () => {
+  it('renders without crashing', () => {
+    renderWithRouter(<FilmContainer isLoading={false} list={[]} />);
+  });
+});
