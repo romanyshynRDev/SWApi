89f4ef4e1cc598bb07b4aa2720339257cd6c3236
Roman <roman1romanyshyn@gmail.com>
2025-03-03 17:47:06 +0200

    fix 1

--- src/pages/Home/MainPage.jsx
+++ src/pages/Home/MainPage.jsx
@@ -1,33 +1,24 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMoviesList } from "../../Service/ApiService";
 import FilmContainer from "./HomePage";
 
 const MainPage = () => {
-  const { list, isLoading } = useSelector(state => state.movies)  
-  const dispatch = useDispatch()
-
+  const { list, isLoading } = useSelector((state) => state.movies);
+  const dispatch = useDispatch();
+  const [increment, setIncrement] = useState(0);
   useEffect(() => {
-    dispatch(getMoviesList())
-  }, [dispatch])
-  
-  return(
-    <FilmContainer list={list} isLoading={isLoading} />
-  )
-}
-export default MainPage
+    dispatch(getMoviesList());
+  }, [dispatch]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return (
+    <>
+      <div>
+        {increment}
+        <button onClick={() => setIncrement((prev) => prev + 2)}>Increment</button>
+      </div>
+      {/* <FilmContainer list={list} isLoading={isLoading} /> */}
+    </>
+  );
+};
+export default MainPage;
